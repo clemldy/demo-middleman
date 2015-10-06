c23a1e9721bf5085ac72686d0e42565b3d19e68d
clement ledey <clement.ledey@gmail.com>
2015-10-06 16:12:49 +0200

    Automated commit at 2015-10-06 14:12:49 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -12660,3 +12660,18 @@ return jQuery;
 
 
 
+
+$(document).ready(function() {
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 300) {
+      $(".navbar-wagon").css({
+        "margin-top": "-70px"
+      });
+    }
+    else {
+      $(".navbar-wagon").css({
+        "margin-top": "0px"
+      });
+    }
+  });
+});
