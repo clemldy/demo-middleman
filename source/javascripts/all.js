3aa3d6495e0272d96e309026cac316a501e95f33
clement ledey <clement.ledey@gmail.com>
2015-10-06 16:11:57 +0200

    scroller coaster

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,18 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 300) {
+      $(".navbar-wagon").css({
+        "margin-top": "-70px"
+      });
+    }
+    else {
+      $(".navbar-wagon").css({
+        "margin-top": "0px"
+      });
+    }
+  });
+});
